59d061db9b924fc8fa4ad8cab5079809ec019fdb
Ivan Garcia <alu0100693737@ull.edu.es>
2014-02-21 18:40:31 +0100

    implementado parte del js

--- js/ini.js
+++ js/ini.js
@@ -1,25 +1,25 @@
-"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
-
-$(document).ready(window.onload);
+"use strict";
 
 $(document).ready(function() {
+   if (window.localStorage && localStorage.initialinput && localStorage.finaloutput)
+   {
+    out.className = 'unhidden';
+    initialinput.innerHTML = localStorage.initialinput;
+    finaloutput.innerHTML = localStorage.finaloutput;
+   }
+   var dropZone = document.getElementById('fileinput');
+   dropZone.addEventListener('dragover', handleDragOver, false);
+   dropZone.addEventListener('drop', handleFileSelect, false);
    $("#fileinput").change(calculate);
 });
 
+// main
 function calculate(evt) {
-  var f;
-  if(evt.type != "drop")
-    f = evt.target.files[0];
-  else{
-    evt.stopPropagation();
-    evt.preventDefault();
-    evt.target.style.background = "none";
-    f = evt.dataTransfer.files[0]; 
-  }
-  
+  var f = evt.target.files;
+
   if (f) {
     var r = new FileReader();
-    r.onload = function(e) { 
+    r.onload = function(e) {
       var contents = e.target.result;
       
       var tokens = lexer(contents);
@@ -29,40 +29,85 @@ function calculate(evt) {
       initialinput.innerHTML = contents;
       finaloutput.innerHTML = pretty;
       
-      if (window.localStorage){ 
-  localStorage.fileinput = f;
-  localStorage.initialinput = contents;
-  localStorage.finaloutput = pretty;
-      } 
+      
+      if (window.localStorage)
+      {
+	  localStorage.initialinput = contents;
+	  localStorage.finaloutput = pretty;
+      }
     }
     r.readAsText(f);
-  } else { 
+  } else {
     alert("Failed to load file");
   }
 }
 
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var files = evt.dataTransfer.files; // FileList object.
+//calculate
+    var f = evt.target.files;
+    var output=[];
+    for(var i=0, f; f=files[i]; i++){
+	if (f) {
+	    var r = new FileReader();
+	    r.onload = function(e) {
+	    var contents = e.target.result;
+      
+	    var tokens = lexer(contents);
+	    var pretty = tokensToString(tokens);
+      
+out.className = 'unhidden';
+initialinput.innerHTML = contents;
+finaloutput.innerHTML = pretty;
+
+if (window.localStorage)
+{
+localStorage.initialinput = contents;
+localStorage.finaloutput = pretty;
+}
+}
+
+
+r.readAsText(f);
+output.push(r);
+} else {
+alert("Failed to load file");
+}
+}
+
+
+  }
+
+  function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+}
+
 function tokensToString(tokens) {
    var r = '';
-   for(var i=0; i < tokens.length; i++) {
-     var t = tokens[i]
+   for(var i in tokens) {
+     var t = tokens[i];
      var s = JSON.stringify(t, undefined, 2);
-     s = _.template(template_ext.innerHTML, {token: t, match: s});
+     s = _.template(externScript.innerHTML, {token: t, match: s});
      r += s;
    }
    return '<ol>\n'+r+'</ol>';
 }
 
 function lexer(input) {
-  var blanks         = /^\s+/;
-  var iniheader      = /^\[([^\]\r\n]+)\]/;
-  var comments       = /^[;#](.*)/;
+  var blanks = /^\s+/;
+  var iniheader = /^\[([^\]\r\n]+)\]/;
+  var comments = /^[;#](.*)/;
   var nameEqualValue = /^([^=;\r\n]+)=([^;\r\n]*)/;
-  var any            = /^(.|\n)+/;
+  var any = /^(.|\n)+/;
 
   var out = [];
   var m = null;
 
-  while (input != ''){
+  while (input != '') {
     if (m = blanks.exec(input)) {
       input = input.substr(m.index+m[0].length);
       out.push({ type : 'blanks', match: m });
@@ -75,7 +120,7 @@ function lexer(input) {
       input = input.substr(m.index+m[0].length);
       out.push({ type: 'comments', match: m });
     }
-    else if (m = nameEqualValue.exec(input)){
+    else if (m = nameEqualValue.exec(input)) {
       input = input.substr(m.index+m[0].length);
       out.push({ type: 'nameEqualValue', match: m });
     }
@@ -90,13 +135,3 @@ function lexer(input) {
   }
   return out;
 }
-
-window.onload = function() {
-  // Si el navegador soporta guardar datos locales y tenemos algun dato que guardar.
-  if (window.localStorage && localStorage.initialinput && localStorage.finaloutput) {
-    document.getElementById("out").className = "unhidden"; 
-    document.getElementById("fileinput").innerHTML = localStorage.fileinput; 
-    document.getElementById("initialinput").innerHTML = localStorage.initialinput;
-    document.getElementById("finaloutput").innerHTML = localStorage.finaloutput;
-  }
-};
